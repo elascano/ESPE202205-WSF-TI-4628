6ab0677473d2dcc5c0f897a87c70f4ac8e409271
HolgerCatucuamba <hfcatucuamba@espe.edu.ec>
2022-08-08 10:47:09 -0500

    WS24

---
+++ workshops/catucuambah/unit3/WS24ChangingImages/src/changingImg.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+function image1(){
+	document.getElementById("chair1").src="img/img1.jfif"
+}
+function image2(){
+	document.getElementById("chair1").src="img/img2.jfif"
+}
+function image3(){
+	document.getElementById("chair1").src="img/img3.jfif"
+}
+function image4(){
+	document.getElementById("chair1").src="img/img4.jfif"
+}
