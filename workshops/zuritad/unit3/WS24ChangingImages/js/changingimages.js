e19d5f475ecb2d6f1bd00a4ece2ea3a33b1ea7d3
dilanzurita <dezurita4@espe.edu.ec>
2022-08-08 10:53:05 -0500

    WS24Changing

---
+++ workshops/zuritad/unit3/WS24ChangingImages/js/changingimages.js
@@ -0,0 +1,12 @@
+function image1(){
+    document.getElementById("bigPicture").src = "images/hp1.png"
+}
+function image2(){
+    document.getElementById("bigPicture").src = "images/hp2.webp"
+}
+function image3(){
+    document.getElementById("bigPicture").src = "images/continua.jpg"
+}
+function image4(){
+    document.getElementById("bigPicture").src = "images/canon1.jfif"
+}
