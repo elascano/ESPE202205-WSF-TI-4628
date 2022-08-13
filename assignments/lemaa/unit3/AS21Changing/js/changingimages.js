1d9cb78ec38f66cfa99b16d85e773f0dff713c38
Allison Camila Lema Villarroel <105320433+aclema24@users.noreply.github.com>
2022-08-12 22:12:19 -0500

    AS21Changing

---
+++ assignments/lemaa/unit3/AS21Changing/js/changingimages.js
@@ -0,0 +1,22 @@
+//javascript to load images 
+function image1() {
+    console.log("artistro");
+    document.getElementById("bigPicture").src = "../images/shoes1.png";
+    
+}
+function image2() {
+    document.getElementById("bigPicture").src = "../images/shoes2.png";
+    console.log("artistro");
+    
+}
+function image3() {
+    document.getElementById("bigPicture").src = "../images/shoes3.png";
+    console.log("artistro");
+    
+}
+function image4() {
+    document.getElementById("bigPicture").src = "../images/shoes4.png";
+    console.log("artistro");
+    
+}
+
