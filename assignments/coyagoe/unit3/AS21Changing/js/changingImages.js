abae309cba1c482cac73bbcd41b186161a8f997f
Eunice Pamela Coyago Arce <eunice.coyago20@gmail.com>
2022-08-14 18:26:56 -0500

    AS20Calculator

---
+++ assignments/coyagoe/unit3/AS21Changing/js/changingImages.js
@@ -0,0 +1,13 @@
+//javascript tp load images
+function image1(){
+    document.getElementById("bigPicture").src="images/keyboard1.webp";
+} 
+function image2(){
+    document.getElementById("bigPicture").src="images/keyboard2.jpg";
+} 
+function image3(){
+    document.getElementById("bigPicture").src="images/keyboard3.jpg";
+} 
+function image4(){
+    document.getElementById("bigPicture").src="images/keyboard4.jpg";
+}
