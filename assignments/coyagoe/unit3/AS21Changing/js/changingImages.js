63cbbee8032f1d844f6acae22c1eb44e51badc53
Eunice Pamela Coyago Arce <eunice.coyago20@gmail.com>
2022-08-14 19:02:29 -0500

    AS21Changing

--- assignments/coyagoe/unit3/AS21Changing/js/changingImages.js
+++ assignments/coyagoe/unit3/AS21Changing/js/changingImages.js
@@ -1,13 +1,21 @@
 //javascript tp load images
-function image1(){
-    document.getElementById("bigPicture").src="images/keyboard1.webp";
-} 
-function image2(){
-    document.getElementById("bigPicture").src="images/keyboard2.jpg";
-} 
-function image3(){
-    document.getElementById("bigPicture").src="images/keyboard3.jpg";
-} 
-function image4(){
-    document.getElementById("bigPicture").src="images/keyboard4.jpg";
+function image(opcion) {
+    switch (opcion) {
+        case 1:
+            console.log("Types");
+            document.getElementById("bigPicture").src="images/keyboard1.webp";
+            break;
+        case 2:
+            console.log("Flexible");
+            document.getElementById("bigPicture").src="images/keyboard2.jpg";
+            break;
+        case 3:
+            console.log("Wireless");
+            document.getElementById("bigPicture").src="images/keyboard3.jpg";
+            break;
+        case 4:
+            console.log("Keys");
+            document.getElementById("bigPicture").src="images/keyboard4.jpg";
+           break;
+    }
 }
