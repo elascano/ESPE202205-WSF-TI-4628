0ce1eecbee05609c915e62bba3a9f00b72a08918
DavidCunguan <decunguan@espe.edu.ec>
2022-08-23 16:02:52 -0500

    AS22CalculatorValidation

---
+++ assignments/cunguand/unit3/AS19Php/registerCustomer.php
@@ -0,0 +1,17 @@
+<head>
+<meta charset="UTF-8">
+    <title>Data</title>
+    <link rel="stylesheet" href="style/css.css">
+</head>
+
+<body>
+    <form action="">
+        <h4>Data</h4>
+        Names: <h2><?php echo $_GET["name"]; ?> </h2><br>
+        Type shoes: <h2><?php echo $_GET["shoes"]; ?> </h2><br>
+        Code: <h2><?php echo $_GET["code"]; ?> </h2><br>
+        Price: <h2><?php echo $_GET["price"]; ?> </h2><br>
+    </form>
+</body>
+
+</html>
