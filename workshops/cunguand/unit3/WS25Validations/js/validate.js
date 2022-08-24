3e50ce3c8a5a0e3be24dd486873d4fc30b5d3357
DavidCunguan <decunguan@espe.edu.ec>
2022-08-23 22:03:13 -0500

    WS28

--- workshops/cunguand/unit3/WS25Validations/js/validate.js
+++ workshops/cunguand/unit3/WS25Validations/js/validate.js
@@ -1,31 +1,39 @@
-var expressions={
-    plate: /^[^FfDd][a-zA-Z]{2}[0-9]{4}$/,
+//JavaScript   
+
+var expressions = {
+    plate: /^[^FfDd] [a-zA-Z]{2}[0-9]{4}$/,
     onlyLetters: /^[a-zA-Z\s_\d]{1,30}$/,
-    model: /^[a-zA-Z\s.-\d]{1,10}$/
+    model:/^[a-zA-Z\s.-\d]{1,10}$/
 }
 
 function validateBrand(){
-    var expRegName= /^[a-zA-ZÑñÁáÉéÍíÓóÚú\s]+$/;
-    var brandName=document.getElementById("brand");
-    if(!expRegName.exec(brandName.value)){
+    var expRegName = /^[a-zA-ZÑnÁáÉéÍíÓóÚúÜü\s]+$/;
+    var brandName = document.getElementById("brand");
+    if (!expRegName.exec(brandName.value)){
         alert("Only write letters.");
+
         return false;
-    }else{
+    } else{
         return true;
     }
 }
 
 function validatePlate(){
-    var plateName=document.getElementById('plate').value;
-    if(!(expressions.plate).test(plateName)){
-        window.alert("Invalid Plate !!!");
+    var plateName = document.getElementById('plate').value;
+    if (!(expressions.plate).test(plateName)){
+        window.alert("Invalid Plate !!");
+        return false;
+    } else{
+        return true;
     }
 }
 
 function validateModel(){
-    let inp=document.getElementById('model').value;
-    if(!(expressions.model).test(inp)){
+    var inp = document.getElementById('model').value;
+    if (!(expressions.plate).test(inp)){
+        window.alert("Invalid Plate !!");
         return false;
+    } else{
+        return true;
     }
-    return true;
 }
